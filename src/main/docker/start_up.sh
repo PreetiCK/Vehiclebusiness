ea8c1d9de15dbb904b208878650ee32382df3398
PreetiCK <preetikhodanpur038@gmail.com>
2015-07-29 10:45:21 +0530

    asdas

--- src/main/docker/start_up.sh
+++ src/main/docker/start_up.sh
@@ -1,6 +1,6 @@
 echo "<?xml version=\"1.0\" encoding=\"utf-8\"?>
 	<Context><WatchedResource>WEB-INF/web.xml</WatchedResource>
-	<Resource name=\"jdbc/VehicleRental\" auth=\"Container\" type=\"javax.sql.DataSource\" username=\"root\" password=\"${DB_PASSWORD}\" driverClassName=\"com.mysql.jdbc.Driver\" url=\"jdbc:mysql://${DB_IP}:${DB_PORT}/${DB}?autoReconnect=true\" maxActive=\"8\" maxIdle=\"4\"/>
+	<Resource name=\"jdbc/VehicleRental\" auth=\"Container\" type=\"javax.sql.DataSource\" username=\"root\" password=\"${DB_PASSWORD}\" driverClassName=\"com.mysql.jdbc.Driver\" url=\"jdbc:mysql://${DB_IP}:${DB_PORT}/${DB}?autoReconnect=true\"  initialSize=\"34\" maxActive=\"-1\" maxIdle=\"-1\" maxWait=\"-1\" minIdle=\"-1\"/>
 	</Context>" > /etc/tomcat7/context.xml ;
 service tomcat7 start ;
 tail -f /var/lib/tomcat7/logs/catalina.out
